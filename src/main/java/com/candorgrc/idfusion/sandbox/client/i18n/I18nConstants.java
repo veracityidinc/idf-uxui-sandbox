61d2b2513e91a7f6a89b18aecb1bec8014c791e8
Norbert Torok <ntorok@grubhub.com>
2018-09-20 15:57:05 +0300

    @designer task

--- src/main/java/com/candorgrc/idfusion/sandbox/client/i18n/I18nConstants.java
+++ src/main/java/com/candorgrc/idfusion/sandbox/client/i18n/I18nConstants.java
@@ -2,7 +2,7 @@ package com.candorgrc.idfusion.sandbox.client.i18n;
 
 /**
  * Interface to represent the constants contained in resource bundle:
- * 	'/home/bpetridean/workspace/idf-sandbox/src/main/resources/com/candorgrc/idfusion/sandbox/client/i18n/I18nConstants.properties'.
+ * 	'/Users/norbert/My stuff/WORK/veracityid/idf-sandbox/src/main/resources/com/candorgrc/idfusion/sandbox/client/i18n/I18nConstants.properties'.
  */
 public interface I18nConstants extends com.google.gwt.i18n.client.Constants {
   
