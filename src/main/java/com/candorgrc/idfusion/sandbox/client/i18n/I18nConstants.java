54fa418f6e2606886bbce2689f13f303f28b1d20
ercangvn <ercangvn@gmail.com>
2023-02-24 20:38:34 +0300

    run project

--- src/main/java/com/candorgrc/idfusion/sandbox/client/i18n/I18nConstants.java
+++ src/main/java/com/candorgrc/idfusion/sandbox/client/i18n/I18nConstants.java
@@ -2,7 +2,7 @@ package com.candorgrc.idfusion.sandbox.client.i18n;
 
 /**
  * Interface to represent the constants contained in resource bundle:
- * 	'/home/bpetridean/workspace/idf-uxui-sandbox/src/main/resources/com/candorgrc/idfusion/sandbox/client/i18n/I18nConstants.properties'.
+ * 	'C:/Users/ercan/Github_C/idf-uxui-sandbox/src/main/resources/com/candorgrc/idfusion/sandbox/client/i18n/I18nConstants.properties'.
  */
 public interface I18nConstants extends com.google.gwt.i18n.client.Constants {
   
